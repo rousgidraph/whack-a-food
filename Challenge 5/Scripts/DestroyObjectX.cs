35478e143da8a102e8e854a9493ae90d5b989709
Rousgidraph <52247003+rousgidraph@users.noreply.github.com>
2021-10-05 21:32:25 +0300

    first upload

---
+++ Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("This method has been called ");
+        Destroy(gameObject); // destroy particle after 2 seconds
+    }
+}
